b45736aa3c71f13b7b42ac016154e8985bc2f5d3
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-03-10 23:12:10 +0000

    Added Microsoft Visual C# Express 2008 based project files for SimpleClient.

---
+++ trunk/simpleclient/source/Flowered.UI.Fullscreen/ScreenManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Flowered.UI.Fullscreen
+{
+    public class ScreenManager
+    {
+        private bool fullscreen = false;
+        private Rectangle bounds;
+        private FormWindowState formWindowState;
+        private FormBorderStyle formBorderStyle;
+        private bool topMost;
+
+        private void Save(Form form)
+        {
+            bounds = form.Bounds;
+            formWindowState = form.WindowState;
+            formBorderStyle = form.FormBorderStyle;
+            topMost = form.TopMost;
+        }
+
+        private void Restore(Form form)
+        {
+            form.Bounds = bounds;
+            form.WindowState = formWindowState;
+            form.FormBorderStyle = formBorderStyle;
+            form.TopMost = topMost;
+
+            fullscreen = false;
+        }
+
+        private void Maximize(Form form)
+        {
+            if (!fullscreen)
+            {
+                fullscreen = true;
+                Save(form);
+
+                form.WindowState = FormWindowState.Maximized;
+                form.FormBorderStyle = FormBorderStyle.None;
+                form.TopMost = true;
+            }
+        }
+
+        public void ToogleFullScreenMode(Form form)
+        {
+            if (!fullscreen)
+            {
+                Maximize(form);
+            }
+            else
+            {
+                Restore(form);
+            }
+        }
+
+        public bool Fullscreen
+        {
+            set
+            {
+                fullscreen = value;
+            }
+            get
+            {
+                return fullscreen;
+            }
+        }
+    }
+}
