da77dc420981af11e192a9bf421d4e3a8d080dd8
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-03-10 23:35:05 +0000

    Added missing files.

---
+++ simpleclient/source/Flowered.UI.SimpleClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Flowered.App.Standalone")]
+[assembly: AssemblyDescription("Flowered Project Standalone Application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Be Flowered!")]
+[assembly: AssemblyProduct("Flowered.App.Standalone")]
+[assembly: AssemblyCopyright("Copyright © 2009 Daniel Schubert")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25b89433-9621-410c-8443-d95d9405ac22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
