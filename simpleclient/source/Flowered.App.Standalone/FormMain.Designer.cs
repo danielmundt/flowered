8d4baed515a25b07532ef3df3a58234caccd7083
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-03-10 23:11:55 +0000

    Added Microsoft Visual C# Express 2008 based project files for SimpleClient.

---
+++ simpleclient/source/Flowered.App.Standalone/FormMain.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Flowered.UI.SimpleClient
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.miFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.miHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.miExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.miAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.miEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.miFullscreen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miFile,
+            this.miEdit,
+            this.miHelp});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // miFile
+            // 
+            this.miFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miExit});
+            this.miFile.Name = "miFile";
+            this.miFile.Size = new System.Drawing.Size(35, 20);
+            this.miFile.Text = "&File";
+            // 
+            // miHelp
+            // 
+            this.miHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miAbout});
+            this.miHelp.Name = "miHelp";
+            this.miHelp.Size = new System.Drawing.Size(40, 20);
+            this.miHelp.Text = "&Help";
+            // 
+            // miExit
+            // 
+            this.miExit.Name = "miExit";
+            this.miExit.Size = new System.Drawing.Size(152, 22);
+            this.miExit.Text = "&Exit";
+            // 
+            // miAbout
+            // 
+            this.miAbout.Name = "miAbout";
+            this.miAbout.Size = new System.Drawing.Size(152, 22);
+            this.miAbout.Text = "&About";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.AllowNavigation = false;
+            this.webBrowser.AllowWebBrowserDrop = false;
+            this.webBrowser.CausesValidation = false;
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser.Location = new System.Drawing.Point(0, 24);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScrollBarsEnabled = false;
+            this.webBrowser.Size = new System.Drawing.Size(292, 249);
+            this.webBrowser.TabIndex = 1;
+            this.webBrowser.Url = new System.Uri("http://www.beflowered.net", System.UriKind.Absolute);
+            this.webBrowser.WebBrowserShortcutsEnabled = false;
+            // 
+            // miEdit
+            // 
+            this.miEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miFullscreen});
+            this.miEdit.Name = "miEdit";
+            this.miEdit.Size = new System.Drawing.Size(37, 20);
+            this.miEdit.Text = "&Edit";
+            // 
+            // miFullscreen
+            // 
+            this.miFullscreen.Name = "miFullscreen";
+            this.miFullscreen.ShortcutKeys = System.Windows.Forms.Keys.F11;
+            this.miFullscreen.Size = new System.Drawing.Size(158, 22);
+            this.miFullscreen.Text = "&Fullscreen";
+            this.miFullscreen.Click += new System.EventHandler(this.miFullscreen_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "FormMain";
+            this.Text = "Flowered";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FormMain_KeyUp);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem miFile;
+        private System.Windows.Forms.ToolStripMenuItem miHelp;
+        private System.Windows.Forms.ToolStripMenuItem miExit;
+        private System.Windows.Forms.ToolStripMenuItem miAbout;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.ToolStripMenuItem miEdit;
+        private System.Windows.Forms.ToolStripMenuItem miFullscreen;
+    }
+}
+
---
+++ simpleclient/source/Flowered.App.Standalone/FormMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using Flowered.UI.Fullscreen;
+
+namespace Flowered.UI.SimpleClient
+{
+    public partial class FormMain : Form
+    {
+        private ScreenManager screenManager;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            InitializeHelpers();
+        }
+
+        private void InitializeHelpers()
+        {
+            screenManager = new ScreenManager();
+        }
+
+        private void FormMain_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.F11)
+            {
+                ToogleFullScreenMode(this);
+            }
+        }
+
+        private void ToogleFullScreenMode(Form form)
+        {
+            screenManager.ToogleFullScreenMode(this);
+            menuStrip.Visible = !screenManager.Fullscreen;
+        }
+
+        private void miFullscreen_Click(object sender, EventArgs e)
+        {
+            ToogleFullScreenMode(this);
+        }
+    }
+}
