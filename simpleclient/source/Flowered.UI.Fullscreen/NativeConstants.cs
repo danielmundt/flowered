16de3d476bfd5403b5d9cf14d1dbbe1d465d01c7
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-03-11 23:18:49 +0000

    Improved fullscreen window with native methods.

---
+++ simpleclient/source/Flowered.UI.Fullscreen/NativeConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flowered.UI.Fullscreen
+{
+    public class NativeConstants
+    {
+        /// SM_CXSCREEN -> 0
+        public const int SM_CXSCREEN = 0;
+
+        /// SM_CYSCREEN -> 1
+        public const int SM_CYSCREEN = 1;
+
+        /// SWP_SHOWWINDOW -> 0x0040
+        public const int SWP_SHOWWINDOW = 64;
+    }
+}
---
+++ simpleclient/source/Flowered.UI.Fullscreen/NativeMethods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Flowered.UI.Fullscreen
+{
+    [StructLayoutAttribute(LayoutKind.Sequential)]
+    public struct HWND__
+    {
+        /// int
+        public int unused;
+    }
+
+    public class NativeMethods
+    {
+        /// Return Type: int
+        ///nIndex: int
+        [DllImportAttribute("user32.dll", EntryPoint = "GetSystemMetrics")]
+        public static extern int GetSystemMetrics(int nIndex);
+
+        /// Return Type: BOOL->int
+        ///hWnd: HWND->HWND__*
+        ///hWndInsertAfter: HWND->HWND__*
+        ///X: int
+        ///Y: int
+        ///cx: int
+        ///cy: int
+        ///uFlags: UINT->unsigned int
+        [DllImportAttribute("user32.dll", EntryPoint = "SetWindowPos")]
+        [return: MarshalAsAttribute(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos([InAttribute()] IntPtr hWnd,
+            [InAttribute()] IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        public static int ScreenX
+        {
+            get
+            {
+                return NativeMethods.GetSystemMetrics(NativeConstants.SM_CXSCREEN);
+            }
+        }
+
+        public static int ScreenY
+        {
+            get
+            {
+                return NativeMethods.GetSystemMetrics(NativeConstants.SM_CYSCREEN);
+            }
+        }
+
+        public static void SetWinFullScreen(IntPtr hwnd)
+        {
+            NativeMethods.SetWindowPos(hwnd, IntPtr.Zero, 0, 0,
+                ScreenX, ScreenY, NativeConstants.SWP_SHOWWINDOW);
+        }
+    }
+}
