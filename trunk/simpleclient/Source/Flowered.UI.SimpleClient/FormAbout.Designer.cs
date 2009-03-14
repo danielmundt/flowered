e4c184bdcb8128e1f20dba62c1e9620e7ab85117
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-03-14 22:51:36 +0000

    Added about dialog.

---
+++ trunk/simpleclient/Source/Flowered.UI.SimpleClient/FormAbout.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Flowered.App.Standalone
+{
+    partial class FormAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
+            this.bntOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bntOk
+            // 
+            this.bntOk.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.bntOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bntOk.Location = new System.Drawing.Point(109, 238);
+            this.bntOk.Name = "bntOk";
+            this.bntOk.Size = new System.Drawing.Size(75, 23);
+            this.bntOk.TabIndex = 1;
+            this.bntOk.Text = "&OK";
+            this.bntOk.UseVisualStyleBackColor = true;
+            this.bntOk.Click += new System.EventHandler(this.bntOk_Click);
+            // 
+            // FormAbout
+            // 
+            this.AcceptButton = this.bntOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.bntOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormAbout";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntOk;
+    }
+}
