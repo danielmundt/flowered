7452442215f82d7b185683893277b259e9437115
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-03-16 20:58:22 +0000

    Added custom control assembly.

---
+++ simpleclient/Source/Flowered.UI.Controls/TransparentPanel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Flowered.UI.Controls
+{
+    public class TransparentPanel : Panel
+    {
+        public TransparentPanel()
+        {
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams createParams = base.CreateParams;
+                createParams.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT
+                return createParams;
+            }
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            // Do not paint background.
+        }
+    }
+}
