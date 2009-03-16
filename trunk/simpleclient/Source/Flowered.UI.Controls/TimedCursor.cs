143fc2eee9438f9df1ae6642a50726742b79afa0
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-03-16 20:58:22 +0000

    Added custom control assembly.

---
+++ trunk/simpleclient/Source/Flowered.UI.Controls/TimedCursor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Flowered.UI.Controls
+{
+    public class TimedCursor
+    {
+        private Timer timer = new Timer();
+        private bool showMouse = true;
+        private bool enabled = false;
+
+        public TimedCursor()
+        {
+            timer.Enabled = false;
+            timer.Interval = 2000;
+            timer.Tick += new System.EventHandler(timer_Tick);
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                return enabled;
+            }
+            set
+            {
+                enabled = value;
+                timer.Enabled = enabled;
+            }
+        }
+
+        public int Timeout
+        {
+            get
+            {
+                return timer.Interval;
+            }
+            set
+            {
+                timer.Interval = value;
+            }
+        }
+
+        public void Show()
+        {
+            if (enabled)
+            {
+                timer.Enabled = true;
+            }
+            if (!this.showMouse)
+            {
+                Cursor.Show();
+                showMouse = true;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (enabled)
+            {
+                timer.Enabled = false;
+
+                if (showMouse)
+                {
+                    Cursor.Hide();
+                    showMouse = false;
+                }
+            }
+        }
+    }
+}
