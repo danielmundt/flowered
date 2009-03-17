5f0d3a330a969d4991a91a25a347e34ef6f50321
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-03-17 22:41:29 +0000

    Added form for setting destination URL.

---
+++ simpleclient/Source/Flowered.UI.SimpleClient/FormAddUrl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Flowered.App.Standalone
+{
+    public partial class FormSetUrl : Form
+    {
+        public FormSetUrl()
+        {
+            InitializeComponent();
+        }
+
+        public string Url
+        {
+            get
+            {
+                return tbUrl.Text;
+            }
+            set
+            {
+                tbUrl.Text = value;
+            }
+        }
+    }
+}
