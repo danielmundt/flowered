35d1abd42899606cc07f053d28d54e42ffa565d3
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-04-19 21:14:08 +0000

    Added new marker definitions.

--- gae/src/static/js/marker.js
+++ gae/src/static/js/marker.js
@@ -1,9 +1,44 @@
 FLOWERED_IMAGES = {
   'f00': {
     'image': '/static/images/map-f00.png',
-    'shadow': '/static/images/map-shadow-f00.png',
+    'shadow': '/static/images/shadow-map-f00.png',
     'iconSize': {'width': '66', 'height': '70'},
     'shadowSize': {'width': '102', 'height': '70'},
     'anchor': {'x': '33', 'y': '35'}
+  },
+  'f01': {
+	'image': '/static/images/map-f01.png',
+	'shadow': '/static/images/shadow-map-f01.png',
+	'iconSize': {'width': '73', 'height': '78'},
+    'shadowSize': {'width': '113', 'height': '78'},
+	'anchor': {'x': '36', 'y': '39'}
+  },
+  'f02': {
+	'image': '/static/images/map-f02.png',
+	'shadow': '/static/images/shadow-map-f02.png',
+	'iconSize': {'width': '86', 'height': '67'},
+	'shadowSize': {'width': '43', 'height': '33'},
+	'anchor': {'x': '33', 'y': '35'}
+  },
+  'f03': {
+	'image': '/static/images/map-f03.png',
+	'shadow': '/static/images/shadow-map-f03.png',
+	'iconSize': {'width': '42', 'height': '47'},
+	'shadowSize': {'width': '66', 'height': '47'},
+	'anchor': {'x': '21', 'y': '23'}
+  },
+  'f04': {
+	'image': '/static/images/map-f04.png',
+	'shadow': '/static/images/shadow-map-f04.png',
+	'iconSize': {'width': '63', 'height': '55'},
+	'shadowSize': {'width': '91', 'height': '55'},
+	'anchor': {'x': '31', 'y': '27'}
+  },
+  'f05': {
+	'image': '/static/images/map-f05.png',
+	'shadow': '/static/images/shadow-map-f05.png',
+	'iconSize': {'width': '82', 'height': '61'},
+	'shadowSize': {'width': '113', 'height': '61'},
+	'anchor': {'x': '41', 'y': '30'}
   }
 };
