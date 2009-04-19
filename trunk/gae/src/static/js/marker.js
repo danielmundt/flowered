54c9f485a5c749127e561bacb8bf0ee99eede8df
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-04-19 17:44:50 +0000

    Renamed image base names.

--- trunk/gae/src/static/js/marker.js
+++ trunk/gae/src/static/js/marker.js
@@ -1,7 +1,7 @@
 FLOWERED_IMAGES = {
   'f00': {
-    'image': '/static/images/f00.png',
-    'shadow': '/static/images/shadow-f00.png',
+    'image': '/static/images/map-f00.png',
+    'shadow': '/static/images/map-shadow-f00.png',
     'iconSize': {'width': '66', 'height': '70'},
     'shadowSize': {'width': '102', 'height': '70'},
     'anchor': {'x': '33', 'y': '35'}
