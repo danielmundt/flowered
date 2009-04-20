3f0b6f23599620b2a29f1cc49f9707d613cf4462
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-04-20 17:48:49 +0000

    Added more flowers to toolbar.

--- gae/src/static/js/marker.js
+++ gae/src/static/js/marker.js
@@ -40,5 +40,47 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '82', 'height': '61'},
 	'shadowSize': {'width': '113', 'height': '61'},
 	'anchor': {'x': '41', 'y': '30'}
-  }
+  },
+  'f06': {
+	'image': '/static/images/map-f06.png',
+	'shadow': '/static/images/shadow-map-f06.png',
+	'iconSize': {'width': '38', 'height': '91'},
+	'shadowSize': {'width': '84', 'height': '91'},
+	'anchor': {'x': '19', 'y': '45'}
+  },
+  'f07': {
+	'image': '/static/images/map-f07.png',
+	'shadow': '/static/images/shadow-map-f07.png',
+	'iconSize': {'width': '58', 'height': '62'},
+	'shadowSize': {'width': '90', 'height': '62'},
+	'anchor': {'x': '29', 'y': '31'}
+  },
+  'f08': {
+	'image': '/static/images/map-f08.png',
+	'shadow': '/static/images/shadow-map-f08.png',
+	'iconSize': {'width': '60', 'height': '83'},
+	'shadowSize': {'width': '102', 'height': '83'},
+	'anchor': {'x': '30', 'y': '41'}
+  },
+  'f09': {
+	'image': '/static/images/map-f09.png',
+	'shadow': '/static/images/shadow-map-f09.png',
+	'iconSize': {'width': '43', 'height': '44'},
+	'shadowSize': {'width': '66', 'height': '44'},
+	'anchor': {'x': '21', 'y': '22'}
+  },
+  'f10': {
+	'image': '/static/images/map-f10.png',
+	'shadow': '/static/images/shadow-map-f10.png',
+	'iconSize': {'width': '60', 'height': '66'},
+	'shadowSize': {'width': '94', 'height': '66'},
+	'anchor': {'x': '30', 'y': '33'}
+  },
+  'f11': {
+	'image': '/static/images/map-f11.png',
+	'shadow': '/static/images/shadow-map-f11.png',
+	'iconSize': {'width': '26', 'height': '27'},
+	'shadowSize': {'width': '40', 'height': '27'},
+	'anchor': {'x': '13', 'y': '13'}
+  } 
 };
