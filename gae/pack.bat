8fccc79482b2533c5462d652edec89700f6ed4f6
daniel.schubert <daniel.schubert@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-04-21 21:07:33 +0000

    Added minified css and js versions. Added yuicompressor jar to tools section. Added pack.bat.

---
+++ gae/pack.bat
@@ -0,0 +1,5 @@
+java -jar .\tools\yuicompressor-2.4.2.jar --type css -o .\src\static\css\flowered-min.css .\src\static\css\flowered.css
+java -jar .\tools\yuicompressor-2.4.2.jar --type css -o .\src\static\css\jquery-ui-min.css .\src\static\css\jquery-ui.css
+java -jar .\tools\yuicompressor-2.4.2.jar --type js -o .\src\static\js\flowered-min.js .\src\static\js\flowered.js
+java -jar .\tools\yuicompressor-2.4.2.jar --type js -o .\src\static\js\marker-min.js .\src\static\js\marker.js
+java -jar .\tools\yuicompressor-2.4.2.jar --type js -o .\src\static\js\toolbar-min.js .\src\static\js\toolbar.js
