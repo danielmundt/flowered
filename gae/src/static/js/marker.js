36ca9dbbeef1643f0202bbdce7d02e701f156c4a
daniel.schubert@gmail.com <daniel.schubert@gmail.com@640d5330-c638-11dd-ac57-e1c5a365c3c4>
2009-06-06 00:26:05 +0000

    Added z-order to markers.

--- gae/src/static/js/marker.js
+++ gae/src/static/js/marker.js
@@ -5,7 +5,7 @@ FLOWERED_IMAGES = {
     'iconSize': {'width': '109', 'height': '71'},
     'shadowSize': {'width': '145', 'height': '71'},
     'anchor': {'x': '54', 'y': '35'},
-    'properties': {'draggable': false, 'deleteable': false}
+    'properties': {'draggable': false, 'deleteable': false, 'zindex': 5 }
   },
   'f00': {
     'image': '/static/images/map-f00.png',
@@ -13,7 +13,7 @@ FLOWERED_IMAGES = {
     'iconSize': {'width': '66', 'height': '70'},
     'shadowSize': {'width': '102', 'height': '70'},
     'anchor': {'x': '33', 'y': '35'},
-    'properties': {'draggable': true, 'deleteable': true}
+    'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f01': {
 	'image': '/static/images/map-f01.png',
@@ -21,7 +21,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '73', 'height': '78'},
     'shadowSize': {'width': '113', 'height': '78'},
 	'anchor': {'x': '36', 'y': '39'},
-    'properties': {'draggable': true, 'deleteable': true}
+    'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f02': {
 	'image': '/static/images/map-f02.png',
@@ -29,7 +29,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '86', 'height': '67'},
 	'shadowSize': {'width': '43', 'height': '33'},
 	'anchor': {'x': '33', 'y': '35'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f03': {
 	'image': '/static/images/map-f03.png',
@@ -37,7 +37,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '42', 'height': '47'},
 	'shadowSize': {'width': '66', 'height': '47'},
 	'anchor': {'x': '21', 'y': '23'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f04': {
 	'image': '/static/images/map-f04.png',
@@ -45,7 +45,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '63', 'height': '55'},
 	'shadowSize': {'width': '91', 'height': '55'},
 	'anchor': {'x': '31', 'y': '27'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f05': {
 	'image': '/static/images/map-f05.png',
@@ -53,7 +53,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '82', 'height': '61'},
 	'shadowSize': {'width': '113', 'height': '61'},
 	'anchor': {'x': '41', 'y': '30'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f06': {
 	'image': '/static/images/map-f06.png',
@@ -61,7 +61,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '38', 'height': '91'},
 	'shadowSize': {'width': '84', 'height': '91'},
 	'anchor': {'x': '19', 'y': '45'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f07': {
 	'image': '/static/images/map-f07.png',
@@ -69,7 +69,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '58', 'height': '62'},
 	'shadowSize': {'width': '90', 'height': '62'},
 	'anchor': {'x': '29', 'y': '31'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f08': {
 	'image': '/static/images/map-f08.png',
@@ -77,7 +77,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '60', 'height': '83'},
 	'shadowSize': {'width': '102', 'height': '83'},
 	'anchor': {'x': '30', 'y': '41'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f09': {
 	'image': '/static/images/map-f09.png',
@@ -85,7 +85,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '43', 'height': '44'},
 	'shadowSize': {'width': '66', 'height': '44'},
 	'anchor': {'x': '21', 'y': '22'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f10': {
 	'image': '/static/images/map-f10.png',
@@ -93,7 +93,7 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '60', 'height': '66'},
 	'shadowSize': {'width': '94', 'height': '66'},
 	'anchor': {'x': '30', 'y': '33'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   },
   'f11': {
 	'image': '/static/images/map-f11.png',
@@ -101,6 +101,6 @@ FLOWERED_IMAGES = {
 	'iconSize': {'width': '26', 'height': '27'},
 	'shadowSize': {'width': '40', 'height': '27'},
 	'anchor': {'x': '13', 'y': '13'},
-	'properties': {'draggable': true, 'deleteable': true}
+	'properties': {'draggable': true, 'deleteable': true, 'zindex': 1 }
   } 
 };
